a70426f17b37d25d8cdb0a68ea5adbc9f60ae6d6
Hrishi Vadje <Hrishi Vadje@DESKTOP-BT2LBEJ>
2022-10-24 15:36:27 +0530

    COMMIT

---
+++ Operators/src/BinaryOperator.java
@@ -0,0 +1,27 @@
+
+public class BinaryOperator {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c=30;
+		int d=40;
+		
+		int add= a+b;  
+		int sub= c-a;
+		int mul= c*d;
+		int div=d/b;
+		int rem=d%b;
+		
+		System.out.println(add);
+		System.out.println(sub);
+		System.out.println(mul);
+		System.out.println(div);
+		System.out.println(rem);
+		
+		
+
+	}
+
+}
