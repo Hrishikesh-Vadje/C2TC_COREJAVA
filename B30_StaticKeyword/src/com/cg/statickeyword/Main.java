cb34d3051fb154a3523198a0daebe1652dc75050
Hrishi Vadje <Hrishi Vadje@DESKTOP-BT2LBEJ>
2022-11-02 19:00:33 +0530

    First commit

---
+++ B30_StaticKeyword/src/com/cg/statickeyword/Main.java
@@ -0,0 +1,20 @@
+package com.cg.statickeyword;
+public class Main {
+
+	public static void main(String[] args) {
+		Student s1 = new Student("Hrishi", 8459933453L, "Hrishi@gmail.com", "jspm ntc", 8.34f);
+		System.out.println(s1.getName()); 
+		
+		 s1.change();
+	    System.out.println(s1.getName());  
+	    System.out.println(s1.getMobile());
+	    System.out.println(s1.getEmail());
+	    
+	    System.out.println(s1.getCollege());
+	    s1.setCollege("TSSM");
+	    System.out.println(s1.getCollege());
+	    System.out.println(s1.getCgpa());
+	}
+
+}
+                               
